d275a90ef00e7e27511a59598072daa82650adff
EC2 Default User <ec2-user@ip-172-26-5-87.ap-northeast-1.compute.internal>
2023-08-27 15:29:01 +0000

    add status

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ module.exports = class extends mofron.class.Event {
             this.name("Tap");
 	    
             /* init config */
-            this.confmng().add("taponly", { type: "boolean" });
+            this.confmng().add("taponly", { type: "boolean", init:false });
+            this.confmng().add("status", { type: "object", init:{ status:null,target:null } });
+            
             let dev = comutl.accdev();
             if (('mobile' === dev) || ('tablet' === dev)) {
                 this.confmng("taponly", true);
@@ -45,24 +47,98 @@ module.exports = class extends mofron.class.Event {
     contents (dom) {
         try {
             let evt_obj = this;
-	    let touch = (e) => {
-	        try {
-		    if (true === evt_obj.taponly()) {
-		        e.preventDefault();
+            let find_id = (dm, id) => {
+                try {
+                    if (dm.id() === id) {
+                        return true;
                     }
-		    let tlist = e.changedTouches;
-		    for (let tidx=0; tidx < tlist.length; tidx++) {
-                        if (dom.id() === tlist[tidx].target.id) {
-			    evt_obj.execListener(e);
-			    break;
-			}
+                    let dm_chd = dm.child();
+                    for (let idx in dm_chd) {
+                        if (true === find_id(dm_chd[idx],id)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                } catch (e) {
+                    console.error(e.stack);
+                    throw e;
+                }
+            }
+	    let touch_start = (e) => {
+                try {
+                    let tlist = e.changedTouches;
+                    for (let tidx=0; tidx<tlist.length; tidx++) {
+                        /* find touched dom */
+                        if (true === find_id(dom, tlist[tidx].target.id)) {
+                            /* hit */
+                            evt_obj.status('start', tlist[tidx].target.id);
+                            break;
+                        }
                     }
 		} catch (e) {
                     console.error(e.stack);
                     throw e;
-		}
-	    }
-            document.addEventListener("touchstart", touch, { passive: false });
+                }
+	    };
+            document.addEventListener("touchstart", touch_start, { passive: false });
+            
+            
+            let touch_move = (e) => {
+                try {
+                    let tlist = e.changedTouches;
+                    for (let tidx=0; tidx<tlist.length; tidx++) {
+                        /* find touched dom */
+                        if (true === find_id(dom, tlist[tidx].target.id)) {
+                            /* hit */
+                            evt_obj.status(null);
+                            break;
+                        }
+                    }
+                } catch (e) {
+                    console.error(e.stack);
+                    throw e;
+                }
+            };
+            document.addEventListener("touchmove", touch_move, { passive: false });
+            
+            
+	    let touch_end = (e) => {
+                try {
+	            let tlist = e.changedTouches;
+                    for (let tidx=0; tidx<tlist.length; tidx++) {
+                        /* find touched dom */
+                        if (true === find_id(dom, tlist[tidx].target.id)) {
+                            /* hit */
+                            let sts = evt_obj.status();
+                            if (('start' === sts.status) && (tlist[tidx].target.id === sts.target)) {
+                                evt_obj.execListener(e);
+                            }
+                            evt_obj.status(null);
+                            break;
+                        }
+                    }
+                } catch (e) {
+                    console.error(e.stack);
+                    throw e;
+                }
+	    };
+	    document.addEventListener("touchend", touch_end, { passive: false });
+
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    status (sts,tgt) {
+        try {
+            if (undefined === sts) {
+                return this.confmng('status');
+            } else if (null === sts) {
+                this.confmng('status', { status:null, target:null });
+            } else {
+                this.confmng('status', { status:sts, target:tgt });
+            }
         } catch (e) {
             console.error(e.stack);
             throw e;
